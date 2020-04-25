e005a749d9578a0d2227fcb6428d8b514805e2cd
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-25 18:21:47 +0900

    interactorにboardを追加

--- Reversi/Interactor.swift
+++ Reversi/Interactor.swift
@@ -3,6 +3,9 @@ import Foundation
 class Interactor {
     /// どちらの色のプレイヤーのターンかを表します。ゲーム終了時は `nil` です。
     var turn: Disk? = .dark
+    
+    #warning("init時にdiskをセットする")
+    var board: Board = .init()
 }
 
 struct DiskPlacementError: Error {
