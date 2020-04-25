982ec274ac1618d549b3f03da14467a1cfcf1075
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-25 17:05:10 +0900

    Diskのindexのテストを追加

--- ReversiTests/DiskTests.swift
+++ ReversiTests/DiskTests.swift
@@ -5,4 +5,14 @@ class DiskTests: XCTestCase {
     func testAllCases() {
         XCTAssertEqual(Disk.allCases, [.dark, .light])
     }
+    
+    func testMakeWithIndex() {
+        XCTAssertEqual(Disk(index: 0), .dark)
+        XCTAssertEqual(Disk(index: 1), .light)
+    }
+    
+    func testGetIndex() {
+        XCTAssertEqual(Disk.dark.index, 0)
+        XCTAssertEqual(Disk.light.index, 1)
+    }
 }
