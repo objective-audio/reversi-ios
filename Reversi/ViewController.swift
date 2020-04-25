21ba6a43f14ac1cd33aefc24f53c934218b604dd
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-25 16:15:44 +0900

    Cancellerを別ファイルに切り出す

--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -450,21 +450,6 @@ extension ViewController {
 
 // MARK: Additional types
 
-final class Canceller {
-    private(set) var isCancelled: Bool = false
-    private let body: (() -> Void)?
-    
-    init(_ body: (() -> Void)?) {
-        self.body = body
-    }
-    
-    func cancel() {
-        if self.isCancelled { return }
-        self.isCancelled = true
-        self.body?()
-    }
-}
-
 struct DiskPlacementError: Error {
     let disk: Disk
     let x: Int
