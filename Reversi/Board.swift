a3bd1703ac615c993964b0dcc3f3a70b1d2821e2
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-25 18:13:55 +0900

    Boardにdiskを保持

--- Reversi/Board.swift
+++ Reversi/Board.swift
@@ -5,4 +5,22 @@ struct Board {
     static let width = 8
     /// 盤の高さ（ `8` ）を返します。
     static let height = 8
+    
+    private(set) var disks: [[Disk?]]
+    
+    init(disks: [[Disk?]]) {
+        self.disks = disks
+    }
+    
+    mutating func setDisks(_ disks: [[Disk?]]) {
+        self.disks = disks
+    }
+    
+    mutating func setDisk(_ disk: Disk?, atX x: Int, y: Int) {
+        self.disks[y][x] = disk
+    }
+    
+    func diskAt(x: Int, y: Int) -> Disk? {
+        return self.disks[y][x]
+    }
 }
