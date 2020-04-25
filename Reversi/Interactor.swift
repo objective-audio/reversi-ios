200e83c6de874327f0443b6f30c8dc0e71ca64ab
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-25 18:53:21 +0900

    Interactorにplayerを保持する

--- Reversi/Interactor.swift
+++ Reversi/Interactor.swift
@@ -4,6 +4,10 @@ class Interactor {
     /// どちらの色のプレイヤーのターンかを表します。ゲーム終了時は `nil` です。
     var turn: Disk? = .dark
     
+    #warning("init時に読み込んだ値をセットする")
+    var darkPlayer: Player = .manual
+    var lightPlayer: Player = .manual
+    
     #warning("init時にdiskをセットする")
     var board: Board = .init()
 }
