a0b23900855cb47c7e253546087faddc48d2ed84
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-27 21:02:25 +0900

    Interactorの関数をprivateにする

--- Reversi/Interactor.swift
+++ Reversi/Interactor.swift
@@ -91,58 +91,6 @@ class Interactor {
         self._began = true
         self.waitForPlayer()
     }
-    
-    /// プレイヤーの行動後、そのプレイヤーのターンを終了して次のターンを開始します。
-    /// もし、次のプレイヤーに有効な手が存在しない場合、パスとなります。
-    /// 両プレイヤーに有効な手がない場合、ゲームの勝敗を表示します。
-    func nextTurn() {
-        guard let currentSide = self.turn else { return }
-
-        let nextSide = currentSide.flipped
-        
-        if self.board.validMoves(for: nextSide).isEmpty {
-            if self.board.validMoves(for: currentSide).isEmpty {
-                self.turn = nil
-            } else {
-                self.turn = nextSide
-                
-                self.delegate?.noPlaceToPutDisk()
-            }
-        } else {
-            self.turn = nextSide
-            self.waitForPlayer()
-        }
-    }
-    
-    func reset() {
-        self.animationCanceller?.cancel()
-        self.animationCanceller = nil
-        
-        for side in Side.allCases {
-            self.playerCancellers[side]?.cancel()
-            self.playerCancellers.removeValue(forKey: side)
-        }
-        
-        self.newGame()
-        self.waitForPlayer()
-    }
-    
-    func setPlayer(_ player: Player, side: Side) {
-        switch side {
-        case .dark:
-            self.darkPlayer = player
-        case .light:
-            self.lightPlayer = player
-        }
-    }
-    
-    func placeDiskByManual(at position: Board.Position) {
-        guard let side = self.turn else { return }
-        if self.isAnimating { return }
-        guard case .manual = self.player(for: side) else { return }
-        
-        try? self.placeDisk(at: position)
-    }
 }
 
 private extension Interactor {
@@ -234,6 +182,58 @@ private extension Interactor {
         if !self.isAnimating, side == self.turn, case .computer = self.player(for: side) {
             self.playTurnOfComputer()
         }
+    }
+    
+    /// プレイヤーの行動後、そのプレイヤーのターンを終了して次のターンを開始します。
+    /// もし、次のプレイヤーに有効な手が存在しない場合、パスとなります。
+    /// 両プレイヤーに有効な手がない場合、ゲームの勝敗を表示します。
+    func nextTurn() {
+        guard let currentSide = self.turn else { return }
+
+        let nextSide = currentSide.flipped
+        
+        if self.board.validMoves(for: nextSide).isEmpty {
+            if self.board.validMoves(for: currentSide).isEmpty {
+                self.turn = nil
+            } else {
+                self.turn = nextSide
+                
+                self.delegate?.noPlaceToPutDisk()
+            }
+        } else {
+            self.turn = nextSide
+            self.waitForPlayer()
+        }
+    }
+    
+    func reset() {
+        self.animationCanceller?.cancel()
+        self.animationCanceller = nil
+        
+        for side in Side.allCases {
+            self.playerCancellers[side]?.cancel()
+            self.playerCancellers.removeValue(forKey: side)
+        }
+        
+        self.newGame()
+        self.waitForPlayer()
+    }
+    
+    func setPlayer(_ player: Player, side: Side) {
+        switch side {
+        case .dark:
+            self.darkPlayer = player
+        case .light:
+            self.lightPlayer = player
+        }
+    }
+    
+    func placeDiskByManual(at position: Board.Position) {
+        guard let side = self.turn else { return }
+        if self.isAnimating { return }
+        guard case .manual = self.player(for: side) else { return }
+        
+        try? self.placeDisk(at: position)
     }
 }
 
