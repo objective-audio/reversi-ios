6cdfe0c079cb9e39ea5198f6d42ecd862ce30385
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-27 20:21:56 +0900

    fix Action

--- Reversi/Action.swift
+++ Reversi/Action.swift
@@ -3,6 +3,8 @@ import Foundation
 enum Action {
     case begin
     case placeDisk(position: Board.Position)
+    case endPlaceDisks
     case changePlayer(_ player: Player, side: Side)
+    case pass
     case reset
 }
