1a02dde6f2f57ef6f1da8cc856b4dbcc83066835
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-29 06:31:51 +0900

    Playerのテストを追加

--- ReversiTests/PlayerTests.swift
+++ ReversiTests/PlayerTests.swift
@@ -2,4 +2,13 @@ import XCTest
 @testable import Reversi
 
 class PlayerTests: XCTestCase {
+    func testRawValue() {
+        XCTAssertEqual(Player.manual.rawValue, 0)
+        XCTAssertEqual(Player.computer.rawValue, 1)
+    }
+    
+    func testFlipped() {
+        XCTAssertEqual(Player.manual.flipped, .computer)
+        XCTAssertEqual(Player.computer.flipped, .manual)
+    }
 }
