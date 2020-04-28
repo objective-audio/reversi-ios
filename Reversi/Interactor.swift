039709c36946914759818d06b3459f0a2d80701c
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-29 00:17:32 +0900

    ActionをInteractorのスコープに移動

--- Reversi/Interactor.swift
+++ Reversi/Interactor.swift
@@ -14,15 +14,6 @@ protocol InteractorEventReceiver: class {
     func receiveEvent(_ event: InteractorEvent)
 }
 
-enum Action {
-    case begin
-    case placeDisk(position: Board.Position)
-    case endPlaceDisks
-    case changePlayer(_ player: Player, side: Side)
-    case pass
-    case reset
-}
-
 class Interactor {
     weak var eventReceiver: InteractorEventReceiver?
     
@@ -259,6 +250,17 @@ private extension Interactor {
         } else {
             self.waitForPlayer(side: nextSide)
         }
+    }
+}
+
+extension Interactor {
+    enum Action {
+        case begin
+        case placeDisk(position: Board.Position)
+        case endPlaceDisks
+        case changePlayer(_ player: Player, side: Side)
+        case pass
+        case reset
     }
 }
 
