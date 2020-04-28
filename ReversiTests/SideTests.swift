9b5202a82d79591dad7f7d1f2dc142467c80c34d
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-29 06:28:56 +0900

    Sideのテストを追加

--- ReversiTests/SideTests.swift
+++ ReversiTests/SideTests.swift
@@ -5,4 +5,14 @@ class SideTests: XCTestCase {
     func testAllCases() {
         XCTAssertEqual(Side.allCases, [.dark, .light])
     }
+    
+    func testFlipped() {
+        XCTAssertEqual(Side.dark.flipped, .light)
+        XCTAssertEqual(Side.light.flipped, .dark)
+    }
+    
+    func testDisk() {
+        XCTAssertEqual(Side.dark.disk, Disk.dark)
+        XCTAssertEqual(Side.light.disk, Disk.light)
+    }
 }
