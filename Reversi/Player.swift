9e0392b7c653d8fdb931f302481bdaf7b43770dd
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-28 21:41:00 +0900

    Playerにflippedを追加

--- Reversi/Player.swift
+++ Reversi/Player.swift
@@ -4,3 +4,12 @@ enum Player: Int {
     case manual = 0
     case computer = 1
 }
+
+extension Player {
+    var flipped: Player {
+        switch self {
+        case .manual: return .computer
+        case .computer: return .manual
+        }
+    }
+}
