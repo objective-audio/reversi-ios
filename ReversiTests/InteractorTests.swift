183c453131bab278a5e63ebff1e0066e615af184
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-29 18:15:49 +0900

    Interactorのテストを追加

--- ReversiTests/InteractorTests.swift
+++ ReversiTests/InteractorTests.swift
@@ -37,4 +37,158 @@ class InteractorTests: XCTestCase {
             XCTFail()
         }
     }
+    
+    func testLoadGame() {
+        let dataStore = DataStoreMock()
+        
+        let disks: [[Disk?]] = [
+            [.light, nil, nil, nil, nil, nil, nil, .dark],
+            [nil, .light, nil, nil, nil, nil, .dark, nil],
+            [nil, nil, .light, nil, nil, .dark, nil, nil],
+            [nil, nil, nil, .light, .dark, nil, nil, nil],
+            [nil, nil, nil, .dark, .light, nil, nil, nil],
+            [nil, nil, .dark, nil, nil, .light, nil, nil],
+            [nil, .dark, nil, nil, nil, nil, .light, nil],
+            [.dark, nil, nil, nil, nil, nil, nil, .light]
+        ]
+        
+        XCTContext.runActivity(named: "turn:light, darkPlayer:manual, lightPlayer:computer") { _ in
+            let expectation = self.expectation(description: "load from DataStore")
+            
+            dataStore.loadHandler = {
+                expectation.fulfill()
+                return .init(turn: .light, darkPlayer: .manual, lightPlayer: .computer, board: disks)
+            }
+            
+            let interactor = Interactor(dataStore: dataStore)
+            
+            self.wait(for: [expectation], timeout: 0.0)
+            
+            XCTAssertEqual(interactor.board.disks, disks)
+            XCTAssertEqual(interactor.darkPlayer, .manual)
+            XCTAssertEqual(interactor.lightPlayer, .computer)
+            
+            if case .launching(let side) = interactor.state {
+                XCTAssertEqual(side, .light)
+            } else {
+                XCTFail()
+            }
+        }
+        
+        XCTContext.runActivity(named: "turn:dark, darkPlayer:computer, lightPlayer:manual") { _ in
+            let expectation = self.expectation(description: "load from DataStore")
+            
+            dataStore.loadHandler = {
+                expectation.fulfill()
+                return .init(turn: .dark, darkPlayer: .computer, lightPlayer: .manual, board: disks)
+            }
+            
+            let interactor = Interactor(dataStore: dataStore)
+            
+            self.wait(for: [expectation], timeout: 0.0)
+            
+            XCTAssertEqual(interactor.board.disks, disks)
+            XCTAssertEqual(interactor.darkPlayer, .computer)
+            XCTAssertEqual(interactor.lightPlayer, .manual)
+            
+            if case .launching(let side) = interactor.state {
+                XCTAssertEqual(side, .dark)
+            } else {
+                XCTFail()
+            }
+        }
+    }
+    
+    func testLoadResultTied() {
+        let dataStore = DataStoreMock()
+        
+        let disks: [[Disk?]] = [
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark]
+        ]
+        
+        let expectation = self.expectation(description: "load from DataStore")
+        
+        dataStore.loadHandler = {
+            expectation.fulfill()
+            return .init(turn: nil, darkPlayer: .computer, lightPlayer: .manual, board: disks)
+        }
+        
+        let interactor = Interactor(dataStore: dataStore)
+        
+        self.wait(for: [expectation], timeout: 0.0)
+        
+        guard case .result(.tied) = interactor.state else {
+            XCTFail()
+            return
+        }
+    }
+    
+    func testLoadResultLightWon() {
+        let dataStore = DataStoreMock()
+        
+        let disks: [[Disk?]] = [
+            [.light, .light, .light, .light, .light, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark]
+        ]
+        
+        let expectation = self.expectation(description: "load from DataStore")
+        
+        dataStore.loadHandler = {
+            expectation.fulfill()
+            return .init(turn: nil, darkPlayer: .manual, lightPlayer: .manual, board: disks)
+        }
+        
+        let interactor = Interactor(dataStore: dataStore)
+        
+        self.wait(for: [expectation], timeout: 0.0)
+        
+        guard case .result(.won(side: .light)) = interactor.state else {
+            XCTFail()
+            return
+        }
+    }
+    
+    func testLoadResultDarkWon() {
+        let dataStore = DataStoreMock()
+        
+        let disks: [[Disk?]] = [
+            [.light, .light, .light, .dark, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark],
+            [.light, .light, .light, .light, .dark, .dark, .dark, .dark]
+        ]
+        
+        let expectation = self.expectation(description: "load from DataStore")
+        
+        dataStore.loadHandler = {
+            expectation.fulfill()
+            return .init(turn: nil, darkPlayer: .manual, lightPlayer: .manual, board: disks)
+        }
+        
+        let interactor = Interactor(dataStore: dataStore)
+        
+        self.wait(for: [expectation], timeout: 0.0)
+        
+        guard case .result(.won(side: .dark)) = interactor.state else {
+            XCTFail()
+            return
+        }
+    }
 }
