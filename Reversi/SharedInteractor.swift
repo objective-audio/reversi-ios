7036e2c8c9fdb559218bc37a5e841379b9fa478f
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-29 23:33:54 +0900

    interactorのsharedを追加

---
+++ Reversi/SharedInteractor.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+extension Interactor {
+    static let shared = Interactor()
+}
--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet private var countLabels: [UILabel]!
     @IBOutlet private var playerActivityIndicators: [UIActivityIndicatorView]!
     
-    private let presenter: Presenter = .init()
+    private let presenter: Presenter = .init(interactor: .shared)
     
     override func viewDidLoad() {
         super.viewDidLoad()
